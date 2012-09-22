f9030ac48f8ba654fe3ab44f145a52fcd4c1cb6a
root <root@www.palladius.eu>
2012-09-22 15:30:50 +0200

    extrapolated cron job into other script...shit!

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # installs a cron job to update this automagically :)
-cat > /etc/cron.d/auto-update-bind <<EOF
+cat > /etc/cron.d/riccardo-auto-update-bind <<EOF
 # this is installed automatically by the dns.palladius.eu gceservice:
 # git@github.com:palladius/gceproject-dns.palladius.eu.git
 
 PATH=\$PATH:/usr/bin/
-*/7 * * * *  root     cd /etc/bind && git pull origin master && service bind9 reload && touch /etc/bind/cron-updated.touch
+ * * * * *  root     cd /etc/bind && git pull origin master && service bind9 reload && touch /etc/bind/cron-updated-direct.touch  || touch /etc/bind/cron-updated-direct-error-$?.touch
+ * * * * *  root     /etc/bind/bin/auto-update-bind
 EOF
-echo Installed crontab in: /etc/cron.d/auto-update-bind 
+echo Installed crontab in: /etc/cron.d/riccardo-auto-update-bind 
