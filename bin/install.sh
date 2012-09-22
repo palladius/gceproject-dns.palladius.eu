a167d3fa8745fff2a5b5e88ce58a93007fa29825
root <root@www.palladius.eu>
2012-09-22 15:26:50 +0200

    cron doesnt work yet

--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,10 @@
 
 # installs a cron job to update this automagically :)
 cat > /etc/cron.d/auto-update-bind <<EOF
+# this is installed automatically by the dns.palladius.eu gceservice:
+# git@github.com:palladius/gceproject-dns.palladius.eu.git
+
+PATH=\$PATH:/usr/bin/
 */7 * * * *  root     cd /etc/bind && git pull origin master && service bind9 reload && touch /etc/bind/cron-updated.touch
 EOF
 echo Installed crontab in: /etc/cron.d/auto-update-bind 
