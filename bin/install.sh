e2c7a42f9a86e286836dad6d7f839a9616b3fff5
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-09-24 17:51:23 +0100

    less aggressive crontab

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+VER=1.1
+
 # installs a cron job to update this automagically :)
+
 cat > /etc/cron.d/riccardo-auto-update-bind <<EOF
+#####################################################################
 # this is installed automatically by the dns.palladius.eu gceservice:
-# git@github.com:palladius/gceproject-dns.palladius.eu.git
+#  git@github.com:palladius/gceproject-dns.palladius.eu.git
+# Install Version: '$VER'
+# Change at your risk/frustration :)
+#####################################################################
 
 PATH=\$PATH:/usr/bin/
- * * * * *  root     cd /etc/bind && git pull origin master && service bind9 reload && touch /etc/bind/cron-updated-direct.touch  || touch /etc/bind/cron-updated-direct-error-$?.touch
- * * * * *  root     /etc/bind/bin/auto-update-bind
+# * * * * *  root     cd /etc/bind && git pull origin master && service bind9 reload && touch /etc/bind/cron-updated-direct.touch  || touch /etc/bind/cron-updated-direct-error-$?.touch
+*/5 * * * *  root     /etc/bind/bin/auto-update-bind
 EOF
+
 echo Installed crontab in: /etc/cron.d/riccardo-auto-update-bind 
